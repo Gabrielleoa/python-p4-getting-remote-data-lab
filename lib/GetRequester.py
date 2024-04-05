efbef4f24d4cf380b7468f516a60a13d74de7d3e
Gabrielle <gabrielle.odhiambo@student.moringaschool.com>
2024-04-04 22:12:01 -0400

    getting remote data

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -5,9 +5,16 @@ class GetRequester:
 
     def __init__(self, url):
         self.url = url
-
+        
     def get_response_body(self):
-        pass
+        url='https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+        response = requests.get(url)
+        return response.content
 
     def load_json(self):
-        pass
+        url= 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+        print(url)
+        response = requests.get(url)
+        return response.json()
+
+        
